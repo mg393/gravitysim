cd6210585a5e5763fb406c125fcddd8ec10e9616
Jay Bizzle <jack.bill.platypus@gmail.com>
2013-06-23 20:18:29 +0100

    Opacity Thing

--- pagescripts.js
+++ pagescripts.js
@@ -1,13 +1,17 @@
 var winWidth = window.innerWidth;
 var posit = (winWidth / 2);
 var vector = 210;
+var opac = 0.15;
+var opacMod = 0.85;
 var inverse = -1;
 vector *= inverse;
 posit += vector;
 $(document).ready(function(){
 	$("#menuBG").click(function(){
-		$(this).animate({left:posit}, 350, 'swing');
-		$("#menuCanvas").animate({left:posit}, 350, 'swing');
+		$(this).animate({left:posit, opacity:opac}, 350, 'swing');
+		$("#menuCanvas").animate({left:posit, opacity:opac}, 350, 'swing');
+		opac += opacMod;
+		opacMod *= inverse;
 		vector *= inverse;
 		posit += vector;
 	});
