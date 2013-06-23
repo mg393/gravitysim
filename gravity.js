7fddfb32d7f91fb4155998a492a57579fb86a942
Michael G <michael_gilmor@live.com>
2013-06-23 12:05:23 +0100

    Some physics groundwork

--- gravity.js
+++ gravity.js
@@ -1,8 +1,19 @@
+//Physics constants and functions
 var G = 6.675e-11
+var objects = [];
 
-function object (radius, mass)
+function object (r, m) //r = radius, m = mass
 {
-    
+    this.radius = r;
+    this.mass = m;
+}
+
+function simulation(c, o) //c = canvas, o = objects 
+{
+    this.canvas = c;
+    this.step = function(){
+        this.draw(c, o);
+    }
 }
 
 function calcForce(mass1, mass2, distance)
@@ -10,10 +21,25 @@ function calcForce(mass1, mass2, distance)
     return ((G*mass1*mass2)/distance^2)
 }
 
-window.onload = function()
+//Page functions
+function draw(c, o) //C = canvas, o = objects (array)
 {
+    //Some kind of interation through objects here while drawing them
+}
+
+function mainloop(sim) //sim = a simulation
+{
+    sim.step();
+    setTimeout('mainLoop()', 35);
+}
+
+window.onload = function()
+{   
+    canvas = document.getElementById("mainCanvas");
+    mainsim = new simulation(canvas);
     while (true) //The main simulation loop
     {
+        mainloop(mainsim);
         return false;
     }
 }
