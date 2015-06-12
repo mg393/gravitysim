8636b32d1fd19220b01cec7157cb6b29c69053a7
Michael G <michaelgilmore39@gmail.com>
2015-06-12 01:41:52 +0100

    Multiple body support added
    Plus some gravity force calcs groundwork

--- gravity.js
+++ gravity.js
@@ -3,6 +3,19 @@ var G = 6.675e-11
 var objects = [];
 var scale = 1000000; //One pixel = 1000km or 1000000m
 
+function calcForce(mass1, mass2, distance)
+{
+    return ((G*mass1*mass2)/distance^2);
+}
+
+function calcAcc(force, mass)
+{
+    return (force/mass);
+}
+
+//Other global values
+var IDcount = 0;
+
 function object(r, m, x, y, hv, vv, ha, va) //r = radius, m = mass, x = x coord, y = y coord, hv = horizontal velocity, vv = vertical velocity, ha = horizontal acceleration, va = vertical acceleration
 {
     this.radius = r;
@@ -13,6 +26,8 @@ function object(r, m, x, y, hv, vv, ha, va) //r = radius, m = mass, x = x coord,
     this.vvelocity = vv;
     this.hacceleration = ha;
     this.vacceleration = va;
+    this.ID = IDcount;
+    IDcount++;
 }
 
 function simulation(c, o, t) //c = canvas, o = objects, t = time between steps
@@ -21,44 +36,57 @@ function simulation(c, o, t) //c = canvas, o = objects, t = time between steps
     this.objects = o;
     this.steptime = t;
     console.log("pants");
+    console.log(o.length);
     this.step = function(){ //use S = ut + 0.5at^2
-        var hvel = o.hvelocity;
-        var vvel = o.vvelocity;
-        var hacc = o.hacceleration;
-        var vacc = o.vacceleration;
+        for (var i = 0; i<o.length; i++) {
+          //Gravity force calcs
+          //for (var i = 0; i <objects.length; i++) {
+            //if (o[i].ID)
+          //}
 
-        console.log("o.vvelocity: " + o.vvelocity + "vvel: " + vvel + "o.vacceleration: " + o.vacceleration)
-        var hdistance = hvel*this.steptime + 0.5*hacc*(this.steptime^2);
-        var vdistance = vvel*this.steptime + 0.5*vacc*(this.steptime^2);
+          //Speed and displacement calcs
+          var hvel = o[i].hvelocity;
+          var vvel = o[i].vvelocity;
+          var hacc = o[i].hacceleration;
+          var vacc = o[i].vacceleration;
 
-        console.log("Vdistance: " + vdistance + " Vacc: " + vacc)
-        o.x += hdistance;
-        o.y += vdistance/* / scale*/;
-        draw(c, o); //needs to be last
+          console.log("o.vvelocity: " + o[i].vvelocity + "vvel: " + vvel + "o.vacceleration: " + o[i].vacceleration)
+          var hdistance = hvel*this.steptime + 0.5*hacc*(this.steptime^2);
+          var vdistance = vvel*this.steptime + 0.5*vacc*(this.steptime^2);
+
+          //console.log("Vdistance: " + vdistance + " Vacc: " + vacc)
+          o[i].x += hdistance / scale;
+          o[i].y += vdistance / scale;
+        }
+
+        //Draw function. Needs to be last
+        draw(c, o);
     }
 }
-
-function calcForce(mass1, mass2, distance)
-{
-    return ((G*mass1*mass2)/distance^2)
-}
-
-//Page functions
-var finished = false;
 
 function draw(c, o) //C = canvas, o = objects (array)
 {
     var context = c.getContext("2d");
-    context.fillStyle="pink";
-    context.strokeStyle="pink";
-    context.beginPath();
-    //console.log(o.x);
-    //console.log(o.y);
-    context.arc(o.x, o.y, o.radius, 0, 2*Math.PI);
-    context.lineWidth = 1;
-    context.fill();
-    context.stroke();
-    //Some kind of interation through objects here while drawing them
+
+    //Clear old stuff:
+    context.clearRect(0, 0, c.width, c.height);
+
+    //TODO: Colour handling or random colour generator
+    //Loop through objects
+    console.log(o.length);
+    for (var i = 0; i<o.length; i++) {
+      //console.log("captain cabinets");
+      //console.log("trapped in cabinets");
+      context.fillStyle="pink";
+      context.strokeStyle="pink";
+      context.beginPath();
+      //console.log(o[i].x);
+      //console.log(o[i].y);
+      context.arc(o[i].x, o[i].y, o[i].radius, 0, 2*Math.PI);
+      context.lineWidth = 1;
+      context.fill();
+      context.stroke();
+    }
 }
 
 function mainloop(sim) //sim = a simulation
@@ -67,15 +95,20 @@ function mainloop(sim) //sim = a simulation
       sim.step();
     } catch(e) {
       console.log("yolo swaggins");
+      console.log(e);
     }
-    setTimeout(function() { mainloop(sim) }, 30)
+    setTimeout(function() { mainloop(sim) }, 30);
 }
 
 window.onload = function() {
     canvas = document.getElementById("mainCanvas");
-    var testObject = new object(6, 100, 100, 101, 10, 10, 1, 1);
+    var testObject = new object(6, 100, 100, 101, 100000, 100000, 1000000, 100000);
+    var testObject2 = new object(8, 500, 500, 450, -5000, -5000, -1000000, -100000);
+
+    var testObjects = [testObject, testObject2];
+    console.log(testObjects.length);
     //TODO: find a way to get mouse x and y from canvas click
-    mainsim = new simulation(canvas, testObject, 5);
+    mainsim = new simulation(canvas, testObjects, 5);
     var intervalID = window.setInterval(mainloop(mainsim), 30);
 
 }
