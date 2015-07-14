9d0917f39f148189a0610e01e863b1c1feb140bc
Michael G <michaelgilmore39@gmail.com>
2015-07-14 16:02:10 +0100

    Base trajectory code implemented

--- scripts/gravity.js
+++ scripts/gravity.js
@@ -42,6 +42,42 @@ function calcAngle(b1, b2) {
     return math.divide(math.multiply(math.atan2(diffX, diffY), 180), math.pi);
 }
 
+function calcTrajectory(i, steptime) { //Returns a math.matrix of the trajectory. i is index in bodies[]
+    var trajectory = [
+        [math.subset(bodies[i].position, math.index(0)), math.subset(bodies[i].position, math.index(1))]
+    ];
+    var force = math.bignumber('0');
+    var totalXForce = math.bignumber('0');
+    var totalYForce = math.bignumber('0');
+
+    for (var v = 0; v < trajectoryLimit; v++) {
+        for (var j = 1; j < bodies.length; j++) {
+            //loop through each body that isn't this, add force from calcForce to total force
+            //use total force to calculate total acceleration and subsequently next x / y coord, add to trajectory
+            force = calcTotalForce(bodies[i], bodies[j], calcDistance(bodies[i], bodies[j]));
+            var angle = calcAngle(bodies[i], bodies[j]);
+            totalXForce = math.add(totalXForce, calcXForce(force, angle));
+            totalYForce = math.add(totalYForce, calcYForce(force, angle));
+        }
+
+        //Set acceleration to one calculated from total X and Y force
+        var totalHAcc = calcAcc(totalXForce, bodies[i].mass);
+        var totalVAcc = calcAcc(totalYForce, bodies[i].mass);
+
+        //Speed and displacement calcs
+        //S = ut + 0.5at^2
+        var hdistance = math.add(math.multiply(b[i].hvelocity, steptime), math.multiply(math.multiply(0.5, b[i].hacceleration), math.pow(steptime, 2)));
+        var vdistance = math.add(math.multiply(b[i].vvelocity, steptime), math.multiply(math.multiply(0.5, b[i].vacceleration), math.pow(steptime, 2)));
+
+        var newX = math.add(hdistance, math.subset(bodies[i].position, math.index(0))); //NOTE: Possible room for error here with +/- values
+        var newY = math.add(vdistance, math.subset(bodies[i].position, math.index(1)));
+
+        trajectory.push([newX, newY]);
+    }
+
+    return math.matrix(trajectory);
+}
+
 function collision(b1, b2) {
     if (math.compare(calcDistance(b1, b2), math.add(b1.radius, b2.radius)) === -1) { //From website: The function cannot be used to compare values smaller than approximately 2.22e-16
         return true;
@@ -67,6 +103,7 @@ var chartData = {
 var graphWriteCount = 0;
 var labelCount = 0;
 var paused = false;
+var trajectoryLimit = 100;
 
 //Other functions
 function addBody(x, y) {
@@ -102,6 +139,7 @@ function body(r, m, x, y, hv, vv, ha, va, s) //r = radius, m = mass, x = x coord
     this.colour = Please.make_color()[0];
     this.ID = IDcount;
     this.s = false; //TODO: implement selection colouring and functions
+    this.trajectory = new math.matrix();
     IDcount++;
 }
 
@@ -117,68 +155,32 @@ function simulation(c, cc, b, t) //c = canvas, cc = chart, b = bodies, t = time 
             for (var i = 0; i < b.length; i++) {
                 var totalVAcc = math.bignumber('0');
                 var totalHAcc = math.bignumber('0');
+                var trajectory = calcTrajectory(i, steptime); //NOTE: Trajectory can stay the same for sim, only need be recalculated if another body added
 
-                for (var j = 0; j < b.length; j++) {
-                    if (i != j) {
-                        if (collision(b[i], b[j]) === true) {
-                            /* var area = math.add(math.multiply(math.pi, math.pow(b[i].radius, 2)), math.multiply(math.pi, math.pow(b[j].radius, 2)));
-                            b[i].radius = math.sqrt(math.divide(area, math.pi));
-                            b[i].mass = math.add(b[j].mass, b[i].mass); */
-                            console.log("collision!");
-                        }
 
-                        //Calculate total force
-                        var force = calcTotalForce(b[i], b[j], calcDistance(b[i], b[j]));
+                /*//logging
+                console.log("totalHAcc: " + math.format(totalHAcc) + " totalVAcc: " + math.format(totalVAcc));
+                console.log("Body IDs: " + b[i].ID + " " + b[j].ID);
+                console.log("X distance: " + math.format(math.subtract(math.subset(b[i].position, math.index(0)), math.subset(b[j].position, math.index(0)))));
+                console.log("Y distance: " + math.format(math.subtract(math.subset(b[i].position, math.index(1)), math.subset(b[j].position, math.index(1)))));
+                console.log("X component = " + xForce + ", Y component = " + yForce);
 
-                        //Calculate angle between bodies
-                        var angle;
-                        try {
-                            angle = calcAngle(b[i], b[j]);
-                        } catch (e) {
-                            console.log("Error in calcAngle(): " + e);
-                        }
-
-                        //Calculate force components
-                        var xForce = calcXForce(force, angle);
-                        var yForce = calcYForce(force, angle);
-
-                        //Calculate acceleration from force components
-                        totalHAcc = math.subtract(totalHAcc, calcAcc(xForce, b[i].mass));
-                        totalVAcc = math.subtract(totalVAcc, calcAcc(yForce, b[i].mass));
-
-                        //logging
-                        console.log("totalHAcc: " + math.format(totalHAcc) + " totalVAcc: " + math.format(totalVAcc));
-                        console.log("Body IDs: " + b[i].ID + " " + b[j].ID);
-                        console.log("X distance: " + math.format(math.subtract(math.subset(b[i].position, math.index(0)), math.subset(b[j].position, math.index(0)))));
-                        console.log("Y distance: " + math.format(math.subtract(math.subset(b[i].position, math.index(1)), math.subset(b[j].position, math.index(1)))));
-                        console.log("X component = " + xForce + ", Y component = " + yForce);
-
-                        //Graphing
-                        if (graphWriteCount >= 10 && b[i].ID === 0) {
-                            cc.addData([parseInt(math.format(xForce))], labelCount);
-                            if (labelCount >= 600) {
-                                cc.removeData();
-                            }
-                            graphWriteCount = 0;
-                        }
+                //Graphing
+                if (graphWriteCount >= 10 && b[i].ID === 0) {
+                    cc.addData([parseInt(math.format(xForce))], labelCount);
+                    if (labelCount >= 600) {
+                        cc.removeData();
                     }
-                }
-
-                //Set acceleration to calculated acceleration
-                b[i].hacceleration = totalHAcc;
-                b[i].vacceleration = totalVAcc;
-
-                //Speed and displacement calcs
-                //S = ut + 0.5at^2
-                var hdistance = math.add(math.multiply(b[i].hvelocity, this.steptime), math.multiply(math.multiply(0.5, b[i].hacceleration), math.pow(this.steptime, 2)));
-                var vdistance = math.add(math.multiply(b[i].vvelocity, this.steptime), math.multiply(math.multiply(0.5, b[i].vacceleration), math.pow(this.steptime, 2)));
+                    graphWriteCount = 0;
+                }*/
 
                 //v = u + at
+                //TODO: Change source of velocity to result of calcTrajectory
                 b[i].hvelocity = math.add(b[i].hvelocity, math.multiply(b[i].hacceleration, this.steptime));
                 b[i].vvelocity = math.add(b[i].vvelocity, math.multiply(b[i].vacceleration, this.steptime));
 
-                b[i].position.subset(math.index(0), math.add(math.subset(b[i].position, math.index(0)), hdistance)); //Set X coord
-                b[i].position.subset(math.index(1), math.add(math.subset(b[i].position, math.index(1)), vdistance)); //And Y
+                b[i].position.subset(math.index(0), math.add(math.subset(b[i].position, math.index(0)), hdistance));
+                b[i].position.subset(math.index(1), math.add(math.subset(b[i].position, math.index(1)), vdistance));
             }
 
             //Add 1 to counts
@@ -237,7 +239,7 @@ window.onload = function() {
 
     //r, m, x, y, hv, vv, ha, va
     var testBody = new body(math.bignumber('15000000'), math.bignumber('1e30'), math.bignumber('200000000'), math.bignumber('150000000'), math.bignumber('0'), math.bignumber('0'), math.bignumber('0'), math.bignumber('0'));
-    var testBody2 = new body(math.bignumber('15000000'), math.bignumber('1500'), math.bignumber('600000000'), math.bignumber('250000000'), math.bignumber('0'), math.bignumber('0'), math.bignumber('0'), math.bignumber('0'));
+    var testBody2 = new body(math.bignumber('15000000'), math.bignumber('1.5e29'), math.bignumber('600000000'), math.bignumber('250000000'), math.bignumber('0'), math.bignumber('0'), math.bignumber('0'), math.bignumber('0'));
 
     bodies.push(testBody);
     bodies.push(testBody2);
